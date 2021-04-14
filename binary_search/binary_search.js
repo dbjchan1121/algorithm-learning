38d67043b0b16698ce799b9bd8f8d1c2778a4396
chenzhe <307400468@qq.com>
2021-04-14 23:40:51 +0800

    二分查找

--- binary_search/binary_search.js
+++ binary_search/binary_search.js
@@ -4,27 +4,21 @@ function binary_search (list, item) {
     let mid = 0, guess = 0
     // console.log(high)
     while (low <= high) {
-        // console.log(high)
-        // high--
-        debugger
-        mid = (low + high)/2
+        mid = Math.floor((low + high)/2)
         guess = list[mid]
-        console.log(guess)
         if (guess === item) {
-            console.log(mid)
             return mid
-        } else if (guess > item) {
-            mid - 1
-            high = mid
-        } else {
-            mid + 1
-            low = mid
-            //console.log(low)
         }
-        // console.log('none')
+        if (guess > item) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
     }
+    return null
 }
 
 let my_list = [1, 3, 5, 7, 9]
 
-console.log(binary_search(my_list, 3))
+console.log(binary_search(my_list, -3))
+console.log(binary_search(my_list, 9))
